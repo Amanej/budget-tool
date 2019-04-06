e293786afe2b061fc3499380d6746a2f2c7d04f1
Aman <aman.mender@tegningtil3d.no>
2019-04-06 22:54:26 +0200

    Basic set up

---
+++ pages/index.vue
@@ -0,0 +1,54 @@
+<template>
+  <section class="container">
+    <div>
+      <div class="mainSplash">
+        <h2>Budsjetter en post</h2>
+        <nuxt-link to="/registrer-inntekt">Registrer inntekt</nuxt-link>
+        <nuxt-link to="/registrer-kostnad">Registrer kostnad</nuxt-link>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+
+.mainSplash > h2 {
+  margin-bottom: 40%;
+  font-size: 28px;
+}
+
+a {
+  margin: 20px;
+}
+</style>
---
+++ pages/registrer-inntekt/index.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="form">
+    <h3>Registrer inntekt</h3>
+    <input type="text" name="kunde" placeholder="Kunde" />
+    <input type="text" name="type" placeholder="Type" />
+    <input type="text" name="sum" placeholder="Sum" />
+    <button type="submit">Registrer inntekt</button>
+</div>
+</template>
+
+<style scoped>
+.form {
+    margin: 20px 15%;
+    display: flex;
+    flex-direction: column;
+}
+.form > h3 {
+    margin-bottom: 20px;
+}
+input {
+    margin: 10px 20px;
+    margin-left: 0;
+    padding: 10px;
+    border: none;
+    font-size: 18px;
+}
+input:focus {
+    outline: none;
+}
+button {
+    margin-left: auto;
+    padding: 10px 20px;
+    border-radius: 8px;
+}
+button:focus {
+    outline: none;
+}
+</style>
